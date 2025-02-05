43e20b5fb9d3970844b45dc102fc59f0cdbc6a11
Ajknight121 <97642775+Ajknight121@users.noreply.github.com>
2025-02-04 18:17:10 -0600

    leave only CSS, add example image

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,10 @@
-import { useState, useEffect } from "react";
-import TodoList from "./TodoList";
 
 function App() {
-  // Load from localStorage on mount
-  useEffect(() => {
-    const savedItems = JSON.parse(localStorage.getItem("data"));
-    if (savedItems) setActivities(savedItems);
-  }, []);
-  const [activities, setActivities] = useState([]);
 
-  const createTask = () => {
-    const title = newTitle;
-    const content = newContent;
-    const newActivites = [...activities, { title: title, content: content }];
-    localStorage.setItem("data", JSON.stringify(newActivites));
-    setActivities(newActivites);
-  };
-
-  const [newTitle, setNewTitle] = useState("");
-  const [newContent, setNewContent] = useState("");
 
   return (
     <>
-      <div className="nav">Welcome!</div>
-
-      <div>
-        <h1>Todo list</h1>
-        <TodoList
-          tasks={activities}
-          setTasks={setActivities}
-        />
-        <div className="create-task">
-          <div>Title</div>
-          <input
-            type="text"
-            value={newTitle}
-            onChange={(e) => setNewTitle(e.target.value)}
-          />
-          <div>Content</div>
-          <input
-            type="text"
-            value={newContent}
-            onChange={(e) => setNewContent(e.target.value)}
-          />
-          <div>
-            <button onClick={createTask}>Create new task</button>
-          </div>
-        </div>
-      </div>
+      <h1>Welcome to React!</h1>
     </>
   );
 }
